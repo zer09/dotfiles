eabbc0d5086c1a412321f1e39a010037f67a3d92
virt <virt@localhost.localdomain>
2017-07-01 22:01:39 +0800

    add creation of fonts directory

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -2,6 +2,7 @@ touch ~/install_progress_log.txt
 log_file=~/install_progress_log.txt
 
 mkdir -p ~/dotfiles-temp/
+mkdir -p ~/.local/share/fonts
 
 . ~/dotfiles/vim-install.sh
 . ~/dotfiles/nerd-fonts-install.sh
