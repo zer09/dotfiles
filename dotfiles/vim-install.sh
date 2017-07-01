64a8981e91bb55cf66ee3aab95c08f88449d7562
virt <virt@localhost.localdomain>
2017-07-01 23:01:49 +0800

    use tee_log on the echo

--- dotfiles/vim-install.sh
+++ dotfiles/vim-install.sh
@@ -1,8 +1,8 @@
 if ! rpm -qa | grep vim-enhanced; then
 	sudo dnf install -y vim-enhanced | tee_log
 	if type -p vim > /dev/null; then
-		echo "vim Installed" >> $log_file
+		echo "vim Installed" | tee_log
 	else
-		echo "vim FAILED TO INSTALL!!!" >> $log_file
+		echo "vim FAILED TO INSTALL!!!" | tee_log
 	fi
 fi
