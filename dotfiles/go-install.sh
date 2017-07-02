fe777ff205d715f41286b6c5dbcd555e5b259700
virt <virt@localhost.localdomain>
2017-07-02 21:55:52 +0800

    go-install.sh

---
+++ dotfiles/go-install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+GO_VERSION="1.8.3"
+GO_DOWNLOAD="https://storage.googleapis.com/golang/go$GO_VERSION.linux-amd64.tar.gz"
+
+GOPATH=$HOME"/go"
+GOBIN=$GOPATH"/bin"
+mkdir -p $GOPATH $GOBIN
+mkdir -p $HOME/go/{src,pkg}
+
+cd $DOT_TEMP
+
+echo "Downloading $GO_DOWNLOAD ..." | tee_log
+curl -fLo "go.tar.gz" $GO_DOWNLOAD | tee_log
+sudo tar -C /usr/local -xzf "go.tar.gz" | tee_log
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bash_profile | tee_log
+echo "export GOPATH=\"\$HOME/go\"" >> ~/.bash_profile | tee_log
+echo "export GOBIN=\"\$GOPATH/bin\"" >> ~/.bash_profile | tee_log
+echo "export PATH=\$PATH:\$GOBIN" >> ~/.bash_profile | tee_log
+echo "Done installing golang..." | tee_log
+
+source ~/.bash_profile
+
+go get -u -v github.com/nsf/gocode | tee_log
+go get -u -v github.com/rogpeppe/godef | tee_log
+go get -u -v golang.org/x/tools/cmd/guru | tee_log
+go get -u -v golang.org/x/tools/cmd/gorename | tee_log
+go get -u -v golang.org/x/tools/cmd/goimports | tee_log
+go get -u -v github.com/alecthomas/gometalinter | tee_log
+go get -u -v github.com/godoctor/godoctor | tee_log
+gometalinter --install --update
+go install github.com/godoctor/godoctor
+
+cd ~
