9e3c177d87598598b13f131df673ab9ffaf9f632
virt <virt@localhost.localdomain>
2017-07-02 19:27:52 +0800

    add --rebase on nerd-fonts

--- dotfiles/nerd-fonts-install.sh
+++ dotfiles/nerd-fonts-install.sh
@@ -2,7 +2,7 @@ cd ~/dotfiles-temp
 git clone --depth 1 git@github.com:ryanoasis/nerd-fonts.git | tee_log
 
 cd ~/dotfiles-temp/nerd-fonts
-git pull | tee_log
+git pull --rebase | tee_log
 ./install.sh DejaVuSansMono | tee_log
 ./install.sh SourceCodePro | tee_log
 ./install.sh Inconsolata | tee_log
